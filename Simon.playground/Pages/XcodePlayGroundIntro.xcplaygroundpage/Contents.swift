8180821a9deab74423f926f5423971644b006af1
Aaron Israel <ami43@drexel.edu>
2017-05-15 13:23:29 -0400

    Both Tutorials Written. needs review.

---
+++ Simon.playground/Pages/XcodePlayGroundIntro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+
+/*:
+# Welcome
+- - -
+ This tutorial will teach you how to use Xcode.
+ \
+ ![XcodeImage](XcodeImage.png)
+ \
+ **Xcode** is an **IDE**, which stands for **I**ntegrated **D**evelopment **E**nvironment. This means that Xcode is a single, convenient hub for your development and testing of code.
+ \
+ ![swiftImage](SwiftImage.png)
+ \
+ **Swift** is a general-purpose, multi-paradigm, compiled programming language developed by Apple for iOS, macOS, watchOS, tvOS, and Linux. This is the Language you will be using. 
+ \
+ ![IntroScreen](IntroScreen.png)
+ \
+ This is how your Xcode screen should look. On the left is the **Navigation Panel** and will help you navigate through files and folders. On the right is the **Assistant Editor** and displays supporting files and views for your primary file. In the middle is the **Primary View** and is the primary file you are currently editing.
+ \
+ ![ViewSettings](ViewSettings.png)
+ Make sure the settings in the top-right corner of the window looks like this.
+  ## Tutorial Guide
+ Below are some examples of things you will see throughout the tutorial.
+ \
+ ![Example_Code_Note](Example_Code_Note.png)
+ \
+ The **Example** section is for code examples.The **Note* section will contain key information. The space inbetween is an example of actual code.
+ \
+ ![ExampleWriteCode](ExampleWriteCode.png)
+ \
+ Here is an example of where to write code You can do so by clicking on the space below the green text or replacing the green text with your code. The green text below the **Start Here** section is called a *comment*.
+ - Note: 
+ A Comment is text written in code that the computer will ignore. It is great for making your code more readable and telling other programmers what your code does.
+ ## Error Guide
+ Here are some **Errors** you are likely to see when working in Xcode.
+ \
+ ![BadError](BadError.png)
+ \
+ This is a **Bad Error** and is noted by a stop-sign with an "!" inside of it. Xcode is telling you something is wrong here but does not know how to fix it. This means your syntax is incorrect or you are doing something you cannot do, try reading the description of the error for some help.
+ \
+ ![GoodError](GoodError.png)
+ \
+ This is a **Good Error** and is noted by a stop-sign with a hexagon inside of it. Xcode knows there is an error and *knows how to fix it*. You can click on the stop-sign to see how Xcode would fix the error. 
+ \
+ ![IgnoreError](IgnoreError.png) 
+ \
+ This is an **Optional Error**  and is noted by a triangle with a circle inside of it. Xcode is telling you that you are doing something that is either not needed or could be done in a better way. For the most part, these can be ignored. 
+ \
+ When you are ready to learn about the game Simon, press [Next.](@next)
+*/
--- Simon.playground/Pages/mySimon.xcplaygroundpage/Contents.swift
+++ Simon.playground/Pages/mySimon.xcplaygroundpage/Contents.swift
@@ -5,7 +5,12 @@ var simon = SimonController (page: PlaygroundPage.current)
 
 /*: 
  # Introduction
- X
+ - - - 
+ In This tutorial you will be learning how to make the game Simon. This game consist of rounds where each round starts when the player hits the **start** button. A round in the game consists of the computer lighting up one or more buttons in a random order, after which the player must reproduce that pattern by pressing the buttons. If the pattern is entered correctly, one button is added to the pattern and the player moves to the next round. If the incorrect pattern is entered then the player will get the chance to retry that round.
+ \
+ On the right is a working version of the game. Play around with it to see how it works. When you are ready get started making your own, press **next**.
+ 
+ [Next](@next)
  */
 
 
